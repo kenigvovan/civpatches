c94a08e48ef12ef41376c92343996f1cb1d33768
kenigvovan <koenigvovan@gmail.com>
2024-01-05 08:45:49 +0100

    Přidejte soubory projektu.

---
+++ civpatches/civpatches/civpatches.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+
+namespace civpatches
+{
+    public class civpatches : ModSystem
+    {
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Event.DidBreakBlock += BreakBlockEvent;
+            api.Event.DidPlaceBlock += PlaceBlockEvent;
+        }
+
+
+        private void BreakBlockEvent(IServerPlayer byPlayer, int oldblockId, BlockSelection blockSel)
+        {
+            ItemSlot offhand = byPlayer?.InventoryManager?.GetHotbarInventory()?[10];
+            var handling = EnumHandHandling.Handled;
+            BlockPos pos = blockSel.Position;
+
+            (offhand?.Itemstack?.Item as ItemPlumbAndSquare)?.OnHeldAttackStart(offhand, byPlayer.Entity, blockSel, null, ref handling);
+            if (handling != EnumHandHandling.Handled)
+            {
+                byPlayer.Entity.World.BlockAccessor.BreakBlock(pos, byPlayer);
+            }
+        }
+
+        private void PlaceBlockEvent(IServerPlayer byPlayer, int oldblockId, BlockSelection blockSel, ItemStack withItemStack)
+        {
+            ItemSlot offhand = byPlayer?.InventoryManager?.GetHotbarInventory()?[10];
+            var handling = EnumHandHandling.Handled;
+            BlockPos pos = blockSel.Position;
+
+            (offhand?.Itemstack?.Item as ItemPlumbAndSquare)?.OnHeldInteractStart(offhand, byPlayer.Entity, blockSel, null, true, ref handling);
+        }
+
+    }
+}
